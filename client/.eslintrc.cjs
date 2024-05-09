56ef555cbb6e5f570a2a4f91941d7bfa8db16c41
Beth Pritchard <bpritchard@scottlogic.com>
2024-05-09 16:33:59 +0100

    edit eslint rules

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,18 +1,17 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended-type-checked'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    project: ['./tsconfig.json'],
   },
-}
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+    },
+  ],
+};
+
