9e9366ccd2eb1d646e389af6ffcc0e489a6486b7
Beth Pritchard <bpritchard@scottlogic.com>
2024-05-10 12:21:08 +0100

    Edit S3 wbsite config

--- infrastructure/client/s3.tf
+++ infrastructure/client/s3.tf
@@ -1,3 +1,37 @@
 resource "aws_s3_bucket" "frontend" {
   bucket = "crossword-scoreboard-frontend"
 }
+
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket              = aws_s3_bucket.frontend.id
+  block_public_acls   = false
+  block_public_policy = false
+}
+
+
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:GetObject"
+        ]
+        Resource = [
+          "${aws_s3_bucket.frontend.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_website_configuration" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+  index_document {
+    suffix = "index.html"
+  }
+}
