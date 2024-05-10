a5dcbdc6a233de483727a11dce40d7b324f1e50e
Beth Pritchard <bpritchard@scottlogic.com>
2024-05-10 14:52:34 +0100

    Test

--- infrastructure/server/main.tf
+++ infrastructure/server/main.tf
@@ -18,7 +18,7 @@ terraform {
 
   backend "s3" {
     bucket         = "crossword-scoreboard-state"
-    key            = "client.tfstate"
+    key            = "server.tfstate"
     region         = "eu-west-2"
     dynamodb_table = "crossword-scoreboard-state-lock"
     encrypt        = true
