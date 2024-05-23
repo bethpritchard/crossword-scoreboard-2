18f2bcf6d50823b65ab648866f5cf1812987941d
Beth Pritchard <bpritchard@scottlogic.com>
2024-05-23 09:44:38 +0100

    defeat the CORS demons

--- client/src/api/updateScore.ts
+++ client/src/api/updateScore.ts
@@ -3,9 +3,13 @@ import axios from 'axios';
 
 const URL = 'https://b9v7lxe1ul.execute-api.eu-west-2.amazonaws.com/v1/db';
 
+const headers = {
+  'Content-Type': 'application/json',
+};
+
 export const updateScore = async (playerScore: PlayerScore) => {
   axios
-    .post(`${URL}`, JSON.stringify(playerScore))
+    .post(`${URL}`, JSON.stringify(playerScore), { headers })
     .then((response) => {
       console.log(response.data.body);
     })
--- lambdas/update-score/main.py
+++ lambdas/update-score/main.py
@@ -1,20 +1,25 @@
 import boto3
 
 dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table("crossword-scoreboard-db")  # Give this to the lambda
+table = dynamodb.Table("crossword-scoreboard-db")
 
 
 def lambda_handler(event, context):
-
     try:
-
         table.update_item(
             Key={"name": event["playerName"]},
             UpdateExpression="set score = :s",
             ExpressionAttributeValues={":s": event["score"]},
             ReturnValues="UPDATED_NEW",
         )
-
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+            "body": "Table updated successfully.",
+        }
     except Exception as e:
         return {
             "statusCode": 500,
@@ -24,12 +29,3 @@ def lambda_handler(event, context):
             },
             "body": str(e),
         }
-
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-        },
-        "body": "Table updated successfully.",
-    }
