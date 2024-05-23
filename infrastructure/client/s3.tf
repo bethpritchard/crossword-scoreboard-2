146b6770d04c8cc86b213608cdf3090f5f7a9d56
Beth Pritchard <bpritchard@scottlogic.com>
2024-05-23 13:47:49 +0100

    Add cloudfront

--- infrastructure/client/s3.tf
+++ infrastructure/client/s3.tf
@@ -1,37 +1,32 @@
-resource "aws_s3_bucket" "frontend" {
-  bucket = "crossword-scoreboard-frontend"
+resource "aws_s3_bucket" "main" {
+  bucket = "${local.project}-frontend"
 }
 
-resource "aws_s3_bucket_public_access_block" "frontend" {
-  bucket              = aws_s3_bucket.frontend.id
-  block_public_acls   = false
-  block_public_policy = false
+resource "aws_s3_account_public_access_block" "main" {
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 
-resource "aws_s3_bucket_policy" "frontend" {
-  bucket = aws_s3_bucket.frontend.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action = [
-          "s3:GetObject"
-        ]
-        Resource = [
-          "${aws_s3_bucket.frontend.arn}/*"
-        ]
-      }
-    ]
-  })
-}
-
-resource "aws_s3_bucket_website_configuration" "frontend" {
-  bucket = aws_s3_bucket.frontend.id
-  index_document {
-    suffix = "index.html"
+data "aws_iam_policy_document" "s3_bucket_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.main.arn}/*"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_cloudfront_distribution.main.arn]
+    }
   }
+}
+
+resource "aws_s3_bucket_policy" "s3_bucket_policy" {
+  bucket = aws_s3_bucket.main.id
+  policy = data.aws_iam_policy_document.s3_bucket_policy.json
 }
